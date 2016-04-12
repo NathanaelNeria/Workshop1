831fc3efeec1c8c20b6f1846fb48fedfd716ef54
NathanaelNeria <nathanaelneria@gmail.com>
2016-04-12 13:56:21 +0800

    Initial commit

---
+++ BMI.py
@@ -0,0 +1,6 @@
+weight = int(input('Enter your weight in kgs:'))
+height = float(input('Enter your height in m:'))
+
+bmi = weight / height**2
+
+print('Therefore, your BMI value is: ', bmi)
---
+++ calcAreaAndVolume.py
@@ -0,0 +1,10 @@
+print('Area Calculator')
+
+width = int(input('Enter width:'))
+height = int(input('Enter height:'))
+depth = int(input('Enter depth:'))
+
+area = width * height
+volume = width * height * depth
+
+print(area,volume)
